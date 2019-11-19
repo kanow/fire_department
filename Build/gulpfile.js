353d8cdadd18911fc77a718881d40598101bd1c3
Karsten Nowak <captnnowi@gmx.de>
2019-11-19 21:50:59 +0100

    [TASK] Get panini working for the templates

--- Build/gulpfile.js
+++ Build/gulpfile.js
@@ -2,7 +2,8 @@ var gulp          = require('gulp');
 var browserSync   = require('browser-sync').create();
 var $             = require('gulp-load-plugins')();
 var autoprefixer  = require('autoprefixer');
-var del = require('del');
+var del           = require('del');
+var panini        = require('panini');
 
 var webpack = require('webpack-stream');
 
@@ -10,9 +11,8 @@ var sassPaths = [
   'node_modules/foundation-sites/scss',
   'node_modules/motion-ui/src'
 ];
-var foundationPath = 'node_modules/foundation-sites/';
 
-function sass() {
+async function sass() {
   return gulp.src('src/scss/app.scss')
     .pipe($.sass({
       includePaths: sassPaths,
@@ -24,18 +24,19 @@ function sass() {
     ]))
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
-};
+}
 
-function serve() {
+async function serve() {
   browserSync.init({
     server: "./dist/"
   });
 
   gulp.watch("src/scss/**/*.scss", sass);
-  gulp.watch("templates/**/*.html", copyHtml).on('change', browserSync.reload);
+  // gulp.watch("pages/**/*.html", copyHtml).on('change', browserSync.reload);
+  gulp.watch(['./src/pages/{layouts,partials,helpers,data}/**/*'], gulp.series([panini.refresh]));
   gulp.watch("src/js/**/*.js", copyJs).on('change', browserSync.reload);
 }
-function copyJs() {
+async function copyJs() {
     return gulp.src(['src/js/**/*.js'])
         .pipe(webpack({
             mode: 'development',
@@ -45,19 +46,30 @@ function copyJs() {
         }))
         .pipe(gulp.dest('dist/js'));
 }
-function copyHtml() {
-    return gulp.src('templates/**/*.html')
-        .pipe(gulp.dest('dist'));
-}
-function copyAssets() {
+// function copyHtml() {
+//     return gulp.src('pages/**/*.html')
+//         .pipe(gulp.dest('dist'));
+// }
+async function copyAssets() {
     return gulp.src('src/assets/**/*')
         .pipe(gulp.dest('dist/assets'));
 }
-function clean() {
+async function clean() {
     return del(['dist']);
+}
+async function pages() {
+    gulp.src('src/pages/**/*.html')
+        .pipe(panini({
+            root: 'src/pages/',
+            layouts: 'src/layouts/',
+            partials: 'src/partials/',
+            helpers: 'src/helpers/',
+            data: 'src/data/'
+        }))
+        .pipe(gulp.dest('dist'));
 }
 
 gulp.task('sass', sass);
 gulp.task('serve', gulp.series('sass', serve));
-gulp.task('copy', gulp.series(copyJs, copyHtml, copyAssets));
-gulp.task('default', gulp.series(clean, 'sass', 'copy', serve));
+gulp.task('copy', gulp.series(copyJs, copyAssets));
+gulp.task('default', gulp.series(clean, gulp.parallel(pages, 'sass', 'copy'), serve));
