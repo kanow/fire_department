5dea12a4995edb0a9f5faca0bb42e135b3afa1e6
Karsten Nowak <captnnowi@gmx.de>
2019-12-23 15:48:37 +0100

    [TASK] Get header image from page properties with cropping

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,19 +1,24 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-call_user_func(function()
-{
-    /**
-     * Temporary variables
-     */
-    $extensionKey = 'firebrigade_theme';
+call_user_func(function ($extensionKey) {
 
-    /**
-     * Default PageTS for FirebrigadeTheme
-     */
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-        $extensionKey,
-        'Configuration/TsConfig/Page/All.tsconfig',
-        'FirebrigadeTheme'
+    $ll = 'LLL:EXT:' . $extensionKey . '/Resources/Private/Language/locallang_db.xlf:';
+
+    $GLOBALS['TCA']['pages']['types']['1']['columnsOverrides']['media']['config']['overrideChildTca']['columns']['crop']['config']['cropVariants'] = array_merge(
+        $GLOBALS['TCA']['pages']['types']['1']['columnsOverrides']['media']['config']['overrideChildTca']['columns']['crop']['config']['cropVariants'] ?? [],
+        [
+            'default' => [
+                'title' => $ll . 'pageHeader.crop_variant.title',
+                'allowedAspectRatios' => [
+                    '3:1' => [
+                        'title' => $ll . 'pageHeader.crop_variant.default',
+                        'value' => 3 / 1
+                    ],
+                ],
+                'selectedRatio' => '3:1',
+            ],
+        ]
     );
-});
+
+}, 'firebrigade_theme');
