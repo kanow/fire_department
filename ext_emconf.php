b26b96bb466741c72c827ec4b3dc292a4742e3e5
Karsten Nowak <captnnowi@gmx.de>
2020-03-22 11:05:38 +0100

    [TASK] Improve ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,27 +4,27 @@
  * Extension Manager/Repository config file for ext "fire_department".
  */
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Fire department Theme',
-    'description' => 'Theme for fire brigade websites with TYPO3',
-    'category' => 'theme',
+    'title' => 'Fire department TYPO3 website theme',
+    'description' => 'Complete TYPO3 site package (theme) to build a website for a fire department. With the extension \"operations\" you can show operations on the website. For latest news are templates for \"news\" extension prepared. The whole template is optimized for mobile devices too.',
+    'category' => 'templates',
+    'author' => 'Karsten Nowak',
+    'author_email' => 'captnnowi@gmx.de',
+    'version' => '1.0.0',
+    'state' => 'beta',
+    'clearCacheOnLoad' => 1,
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-9.5.99',
         ],
-        'conflicts' => [
+        'suggest' => [
+            'operations' => '4.0.0 - 0.0.0'
         ],
     ],
+    'uploadfolder' => 0,
+    'createDirs' => '',
     'autoload' => [
         'psr-4' => [
             'Kanow\\FireDepartment\\' => 'Classes'
         ],
     ],
-    'state' => 'alpha',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'clearCacheOnLoad' => 1,
-    'author' => 'Karsten Nowak',
-    'author_email' => 'captnnowi@gmx.de',
-    'author_company' => 'Kanow',
-    'version' => '1.0.0',
 ];
